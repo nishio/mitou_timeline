8abc89df2b4fa1e822278c803580cc0c44256339
NISHIO Hirokazu <nishio.hirokazu@gmail.com>
2016-02-03 14:39:19 +0900

    divide into peaces

--- lr.py
+++ lr.py
@@ -5,27 +5,39 @@ from scipy.stats import logistic
 import features
 fs = features.POSITIVE + features.NEGATIVE
 M = len(fs)
-pos_x = file('positive.txt').readlines()
-neg_x = file('negative.txt').readlines()#[:len(pos_x)]
-N = len(pos_x) + len(neg_x)
 
-X = np.zeros((N, M))
-Y = np.zeros((N, ))
+def make_feature_matrix(lines):
+    N = len(lines)
+    X = np.zeros((N, M))
+    for i, line in enumerate(lines):
+        X[i, :] = [f in line for f in fs]
+    return X
 
-for i, line in enumerate(pos_x):
-    X[i, :] = [f in line for f in fs]
-    Y[i] = 1
+def learn(penalty='l2'):
+    pos_x = file('positive.txt').readlines()
+    neg_x = file('negative.txt').readlines()#[:len(pos_x)]
+    N = len(pos_x) + len(neg_x)
 
-offset = len(pos_x)
-for i_, line in enumerate(neg_x):
-    i = offset + i_
-    X[i, :] = [f in line for f in fs]
-    Y[i] = 0
+    X = np.zeros((N, M))
+    Y = np.zeros((N, ))
 
-lr = LogisticRegression(penalty='l1')
+    for i, line in enumerate(pos_x):
+        X[i, :] = [f in line for f in fs]
+        Y[i] = 1
 
-lr.fit(X, Y)
-Z = lr.predict_proba(X)[:, 1]
-scores = list(sorted(zip(lr.coef_.ravel(), fs), reverse=True))
-for w, t in scores:
-    print w, t
+    offset = len(pos_x)
+    for i_, line in enumerate(neg_x):
+        i = offset + i_
+        X[i, :] = [f in line for f in fs]
+        Y[i] = 0
+
+    lr = LogisticRegression(penalty=penalty)
+    lr.fit(X, Y)
+    return lr
+
+def feature_compression():
+    lr = learn(penalty='l1')
+    Z = lr.predict_proba(X)[:, 1]
+    scores = list(sorted(zip(lr.coef_.ravel(), fs), reverse=True))
+    for w, t in scores:
+        print w, t
